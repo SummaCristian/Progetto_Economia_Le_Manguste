87a45cb6cae0011b8a258dc7e6fd36fd84dfec16
Cristian Summa <cristian.summa@mail.polimi.it>
2024-05-23 14:26:23 +0200

    Added Tablet support to the Dashboard

--- app/src/main/java/it/polimi/manguste/MainActivity.java
+++ app/src/main/java/it/polimi/manguste/MainActivity.java
@@ -10,6 +10,7 @@ import android.view.WindowManager;
 import android.window.OnBackInvokedDispatcher;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigationrail.NavigationRailView;
 
 import androidx.activity.OnBackPressedCallback;
 import androidx.annotation.NonNull;
@@ -36,7 +37,8 @@ private ActivityMainBinding binding;
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
-        BottomNavigationView navView = findViewById(R.id.nav_view);
+        BottomNavigationView bottomNavView = findViewById(R.id.bottom_nav_view);
+        NavigationRailView navRailView = findViewById(R.id.nav_rail_view);
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
@@ -44,7 +46,19 @@ private ActivityMainBinding binding;
                 .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
         //NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
-        NavigationUI.setupWithNavController(binding.navView, navController);
+        boolean isTablet = getResources().getBoolean(R.bool.isTablet);
+
+        if (isTablet) {
+            // For tablet
+            if (navRailView != null) {
+                NavigationUI.setupWithNavController(navRailView, navController);
+            }
+        } else {
+            // For phone
+            if (bottomNavView != null) {
+                NavigationUI.setupWithNavController(bottomNavView, navController);
+            }
+        }
 
         // Sets the StatusBar to transparent and the BottomNavBar selector color to light/dark green
         Window window = getWindow();
@@ -52,11 +66,20 @@ private ActivityMainBinding binding;
         winParams.flags &= ~WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS;
         window.setAttributes(winParams);
         if ((getResources().getConfiguration().uiMode & Configuration.UI_MODE_NIGHT_MASK) == Configuration.UI_MODE_NIGHT_NO) {
-            window.getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN | View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR);
-            navView.setItemActiveIndicatorColor(ColorStateList.valueOf(Color.parseColor("#92D769")));
+            window.getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN | View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION);
+            if (isTablet) {
+                navRailView.setItemActiveIndicatorColor(ColorStateList.valueOf(Color.parseColor("#92D769")));
+
+            } else {
+                bottomNavView.setItemActiveIndicatorColor(ColorStateList.valueOf(Color.parseColor("#92D769")));
+            }
         } else {
-            window.getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
-            navView.setItemActiveIndicatorColor(ColorStateList.valueOf(Color.parseColor("#385f1c")));
+            window.getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION);
+            if (isTablet) {
+                navRailView.setItemActiveIndicatorColor(ColorStateList.valueOf(Color.parseColor("#385f1c")));
+            } else {
+                bottomNavView.setItemActiveIndicatorColor(ColorStateList.valueOf(Color.parseColor("#385f1c")));
+            }
         }
     }
 
